c551aa6440453a4de02db60bca781e1241bd86ac
tily <tidnlyam@gmail.com>
2014-11-26 17:21:44 +0900

    add Main class

---
+++ src/main/java/tokyo/prgrphs/Main.java
@@ -0,0 +1,9 @@
+package tokyo.prgrphs;
+import tokyo.prgrphs.HelloWorld;
+
+class Main {
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.sayHello(args[0]);
+	}
+}
